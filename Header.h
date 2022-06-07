695521ad45fc65f26b2a38c505658fa559e3fbed
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-07 21:44:44 +0500

    Some Description of Code and Variables added as Comments

--- Header.h
+++ Header.h
@@ -5,6 +5,19 @@
 using namespace std;
 
 
+            // Description of Variables and Code.
+/*
+        Housestart: Starting Boundary of House
+        Houseend  : Ending Boundary of House
+        appleTree : Position of Apple Tree
+        orangeTree: Position of Orange Tree
+  vector of apples: Position of Apple dropped from tree
+  vector of orange: Position of Oranges dropped from oranges
+
+        In this code we are finding number of Apples and Oranges 
+        fall in the boundary of House.
+*/
+
 void countApplesAndOranges(int Housestart, int Houseend, int appleTree, int orangeTree, vector<int> apples, vector<int> oranges) {
     int totalApples, totalOranges;
     totalApples = totalOranges = 0;
